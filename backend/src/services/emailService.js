a0f582c01ad6e3575daaf567c068d6b344b1e765
Yuono Dwi Raharjo <132055549+YuonoD020602@users.noreply.github.com>
2026-06-16 07:38:07 +0700

    Change FROM_EMAIL address in emailService.js

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -1,7 +1,7 @@
 const { Resend } = require('resend');
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const FROM_EMAIL = 'onboarding@resend.dev';
+const FROM_EMAIL = 'noreply@lyraac.site';
 
 const sendEmail = async ({ to, subject, html }) => {
   const { error } = await resend.emails.send({
